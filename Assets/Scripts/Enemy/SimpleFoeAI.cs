4b719a10d65f239e96f0c1bb5fd281bafbd9267b
Calhraviril <97947786+Calhraviril@users.noreply.github.com>
2022-04-20 10:24:22 +0300

    Foe AI Improvement
    Made the AI of enemies less lazily made

--- Assets/Scripts/Enemy/SimpleFoeAI.cs
+++ Assets/Scripts/Enemy/SimpleFoeAI.cs
@@ -6,19 +6,34 @@ public class SimpleFoeAI : MonoBehaviour
 {
     private Transform selfPST;
     private Transform targetPST;
+    private Rigidbody rigi;
     
     [Header("Movement values")]
     [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
 
     private void Start()
     {
         selfPST = gameObject.transform;
-        targetPST = GameObject.Find("Player").transform;
+        targetPST = GameObject.Find("Targeted").transform;
+        rigi = gameObject.GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
         float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(selfPST.position, targetPST.position, step);
+        //Vector3 walke = new Vector3(selfPST.position.x, 0, selfPST.position.z);
+        //Vector3 walky = new Vector3(targetPST.position.x, 0, targetPST.position.z);
+        gameObject.transform.LookAt(GameObject.Find("Player").transform);
+
+        if (rigi.velocity.x < maxSpeed || rigi.velocity.y < maxSpeed || rigi.velocity.z < maxSpeed)
+        {
+            Vector3 Wail = (targetPST.position - selfPST.position) * speed;
+            rigi.AddForce(Wail, ForceMode.Impulse);
+        }
+        else
+        {
+            rigi.velocity = Vector3.zero;
+        }
     }
 }
